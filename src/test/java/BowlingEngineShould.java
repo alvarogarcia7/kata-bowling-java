0a91b659aa967a69f81c5039c2afc178b10cabed
alvaro garcia <derecerca@gmail.com>
2015-03-24 20:36:44 +0100

    refactor: rename

--- src/test/java/BowlingEngineShould.java
+++ src/test/java/BowlingEngineShould.java
@@ -10,31 +10,31 @@ import static org.junit.Assert.assertThat;
  */
 public class BowlingEngineShould {
 
-	private BowlingEngine bowlingEngine;
+	private BowlingEngine sut;
 
 	@Before
 	public void setUp () throws Exception {
-		bowlingEngine = new BowlingEngine();
+		sut = new BowlingEngine();
 	}
 
 	@Test
 	public void return_0_when_no_pins_touched(){
-		assertThat(bowlingEngine.throwBalls(0, 0), is(0));
+		assertThat(sut.throwBalls(0, 0), is(0));
 	}
 
 	@Test
 	public void display_1_when_the_first_ball_drops_1_pin(){
-		assertThat(bowlingEngine.throwBalls(1, 0), is(1));
+		assertThat(sut.throwBalls(1, 0), is(1));
 	}
 
 	@Test
 	public void display_1_when_the_second_ball_drops_1_pin(){
-		assertThat(bowlingEngine.throwBalls(0, 1), is(1));
+		assertThat(sut.throwBalls(0, 1), is(1));
 	}
 
 	@Test
 	public void not_mind_about_the_balls_results_order(){
-		assertThat(bowlingEngine.throwBalls(0, 1), is(bowlingEngine.throwBalls(1, 0)));
+		assertThat(sut.throwBalls(0, 1), is(sut.throwBalls(1, 0)));
 	}
 
 }
