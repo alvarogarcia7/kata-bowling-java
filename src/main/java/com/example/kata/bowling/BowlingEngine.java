9919fd4afeef06932d16bb370132115cc152e6fc
alvaro garcia <derecerca@gmail.com>
2015-03-24 20:57:36 +0100

    green

--- src/main/java/com/example/kata/bowling/BowlingEngine.java
+++ src/main/java/com/example/kata/bowling/BowlingEngine.java
@@ -5,7 +5,26 @@ package com.example.kata.bowling;
 */
 public class BowlingEngine {
 
+	private Integer totalScore;
+
+	public BowlingEngine(){
+		totalScore=0;
+	}
+
 	public Integer throwBalls (int ball1, int ball2) {
+
+		int realValueBall1 = ball1;
+
+		if (totalScore != 0){
+			realValueBall1 *= 2;
+		}
+
+		totalScore += realValueBall1 + ball2;
+
 		return ball1 + ball2;
+	}
+
+	public Integer getFrameScore(){
+		return totalScore;
 	}
 }
