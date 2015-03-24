4de50df5733dcb4552559a28a438d2de57fda90d
alvaro garcia <derecerca@gmail.com>
2015-03-24 20:09:26 +0100

    refactor: extract method object
    split production code from test code

---
+++ src/main/java/com/example/kata/bowling/BowlingEngine.java
@@ -0,0 +1,15 @@
+package com.example.kata.bowling;
+
+/**
+* Created by alvaro on 24/03/15.
+*/
+public class BowlingEngine {
+
+	public Integer throwBalls (final int onlyBall) {
+		return 1;
+	}
+
+	public Integer throwBalls (int ball1, int ball2) {
+		return 0;
+	}
+}
--- src/test/java/BowlingEngineShould.java
+++ src/test/java/BowlingEngineShould.java
@@ -1,3 +1,4 @@
+import com.example.kata.bowling.BowlingEngine;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -10,15 +11,12 @@ public class BowlingEngineShould {
 
 	@Test
 	public void return_0_when_no_pins_touched(){
-		assertThat(throwBalls(0,0), is(0));
+		assertThat(new BowlingEngine().throwBalls(0, 0), is(0));
 	}
 
 	@Test
 	public void display_1_when_the_first_ball_drop_1_pin(){
-		assertThat(throwBalls(1), is(1));
+		assertThat(new BowlingEngine().throwBalls(1), is(1));
 	}
 
-	private int throwBalls(int ball1, int ball2){
-		return 0;
-	}
 }
