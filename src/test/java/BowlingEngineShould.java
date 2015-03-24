dd859701af527ceb0d4086e69d1fda78c9f90c8c
alvaro garcia <derecerca@gmail.com>
2015-03-24 20:35:24 +0100

    refactor: DRY - extract @before

--- src/test/java/BowlingEngineShould.java
+++ src/test/java/BowlingEngineShould.java
@@ -1,4 +1,5 @@
 import com.example.kata.bowling.BowlingEngine;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -9,24 +10,31 @@ import static org.junit.Assert.assertThat;
  */
 public class BowlingEngineShould {
 
+	private BowlingEngine bowlingEngine;
+
+	@Before
+	public void setUp () throws Exception {
+		bowlingEngine = new BowlingEngine();
+	}
+
 	@Test
 	public void return_0_when_no_pins_touched(){
-		assertThat(new BowlingEngine().throwBalls(0, 0), is(0));
+		assertThat(bowlingEngine.throwBalls(0, 0), is(0));
 	}
 
 	@Test
 	public void display_1_when_the_first_ball_drops_1_pin(){
-		assertThat(new BowlingEngine().throwBalls(1, 0), is(1));
+		assertThat(bowlingEngine.throwBalls(1, 0), is(1));
 	}
 
 	@Test
 	public void display_1_when_the_second_ball_drops_1_pin(){
-		assertThat(new BowlingEngine().throwBalls(0, 1), is(1));
+		assertThat(bowlingEngine.throwBalls(0, 1), is(1));
 	}
 
 	@Test
 	public void not_mind_about_the_balls_results_order(){
-		assertThat(new BowlingEngine().throwBalls(0, 1), is(new BowlingEngine().throwBalls(1, 0)));
+		assertThat(bowlingEngine.throwBalls(0, 1), is(bowlingEngine.throwBalls(1, 0)));
 	}
 
 }
